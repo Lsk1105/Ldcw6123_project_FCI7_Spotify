c144f5c600f5d6f632aaaddf9b0699cd2f7eaa22
Lsk1105 <lskung2004@gmail.com>
2025-09-16 15:29:26 +0800

    Initial project setup with main.cpp and Song struct

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//Subject: LDCW6123 - FUNDAMENTALS OF DIGITAL COMPETENCE FOR PROGRAMMER
+//Topic: Spotify
+//Title program: Music Recommendation Assistant 
+//Inputs: Music preference 
+//Outputs: A suggested music title and its artist
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Song {
+    string title;
+    string artist;
+    vector<string> genres;
+};
+
+int main() {
+    return 0;
+}
